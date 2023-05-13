af2d5e5456c410dc43f7daf05c1b5fdc808ae6db
Noah Kim <nykim@usc.edu>
2023-05-12 17:06:35 -0700

    firebase added

--- marketplace/backend/server/index.js
+++ marketplace/backend/server/index.js
@@ -1,16 +1,52 @@
-// server/index.js
+//firebase, firestore imports
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
-const express = require("express");
+//account key
+const serviceAccount = require('../marketplace-f5119-22762f894698.json');
 
-const PORT = process.env.PORT || 3001;
-
-const app = express();
-
-//testing
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+//initial application
+initializeApp({
+  credential: cert(serviceAccount)
 });
 
+//get the firestore database
+const db = getFirestore();
+
+//express setup
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//====== FUNCTIONS =======
+
+//insert into items collection
+async function addItem(){
+  
+  const data = {
+    name: 'Los Angeles',
+    state: 'CA',
+    country: 'USA'
+  };
+  
+  // Add a new document in collection "cities" with ID 'LA'
+  const res = await db.collection('items').doc('LA').set(data);
+
+}
+
+//====== ENDPOINTS =======
+
+//insert a dummy document into the items collection in firebase
+app.get("/addItem", (req, res) => {
+  addItem();
+  res.json({ message: "Item has been added" });
+});
+
+//login endpoint
+
+//create account endpoint
+
+//upload product endpoint
 
 //listening
 app.listen(PORT, () => {
