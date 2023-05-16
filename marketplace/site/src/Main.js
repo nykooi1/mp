53eb33dae74bf28bf57a5c7609455c404a7e83b8
Noah Kim <nykim@usc.edu>
2023-05-15 17:06:27 -0700

    routing updates

---
+++ marketplace/site/src/Main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route}
+    from 'react-router-dom';
+
+//pages
+import Home from './Home';
+import AddItem from './formComponents/AddItem';
+
+const Main = () => {
+  return (
+    <Router>
+      <Routes> {/* The Routes decides which component to show based on the current URL.*/}
+        <Route exact path='/' element={<Home/>}></Route>
+        <Route exact path='/AddItem' element={<AddItem/>}></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default Main;
