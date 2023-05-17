eaf3e5f8a1723be0fd225bb7b3dfc14955b08487
Jasmine Lee <jlee6488@usc.edu>
2023-05-16 19:10:19 -0700

    added signup and login

---
+++ marketplace/site/src/formComponents/LogIn.js
@@ -0,0 +1,57 @@
+import { useState } from "react"
+
+
+const LogIn = () => {
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const payload = {username: username, password: password};
+        const res = await fetch('http://localhost:3001/LogIn', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(payload)
+
+        })
+
+        const data = await res.json()
+        console.log('response:', data)
+        
+    }
+
+  return (
+
+    <div className='login-page-container'>
+        <h2>Login Page</h2>
+         <form className='login-form'>
+            <div className='form-control'>
+                <label>Username</label>
+                <input
+                    type='text'
+                    placeholder='Username'
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}/>
+            </div>
+            <div className='form-control'>
+                <label>Password</label>
+                <input
+                    type='text'
+                    placeholder='Password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}/>
+            </div>
+            <input 
+            type='submit' 
+             value='Login'/>
+        </form>
+        
+    </div>
+  )
+}
+
+export default LogIn
